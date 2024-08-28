c79847d8949a124ab77add6e0292466105478b4d
amirafathy-tech <amira.fathy@solex.tech>
2024-08-28 17:12:33 +0300

    Execution-Order-App

---
+++ src/app/execution-order/execution-order.model.ts
@@ -0,0 +1,77 @@
+
+export class SubItem {
+    Type:string='';
+
+    executionOrderSubCode?: number=0;
+   // mainItemCode?: number;
+    serviceNumberCode?: number;
+    description?: string;
+    unitOfMeasurementCode?:string;
+    currencyCode?: string;
+
+    materialGroupCode?:string;
+    serviceTypeCode?:string;
+    personnelNumberCode?:string;
+    lineTypeCode?:string;
+
+    totalQuantity?: number;
+    amountPerUnit?: number;
+    total: number=0;
+
+    externalServiceNumber?:string;
+    serviceText?:string;
+    lineText?:string;
+    lineNumber?:string;
+
+    biddersLine?:boolean;
+    supplementaryLine?:boolean;
+    lotCostOne?:boolean;
+    doNotPrint?:boolean;
+
+
+    selected?: boolean;
+
+   
+}
+
+export class MainItem {
+
+    Type:string='';
+
+    executionOrderMainCode: number=0;
+    invoiceMainItemCode?: number;
+    serviceNumberCode?: number;
+    description?: string;
+
+    unitOfMeasurementCode?:string;
+    currencyCode?: string;
+    materialGroupCode?:string;
+    serviceTypeCode?:string;
+    personnelNumberCode?: string;
+    lineTypeCode?:string;
+
+    
+    totalQuantity?: number;
+    amountPerUnit?: number;
+    total: number=0;
+
+    actualQuantity?: number;
+    actualPercentage?: number;
+    overFulfillmentPercentage?: number;
+    unlimitedOverFulfillment?: boolean;
+    manualPriceEntryAllowed?: boolean;
+    externalServiceNumber?: string;
+    serviceText?:string;
+    lineText?: string;
+    lineNumber?:string;
+
+
+    biddersLine?:boolean;
+    supplementaryLine?:boolean;
+    lotCostOne?:boolean;
+    doNotPrint?:boolean;
+
+    selected?: boolean;
+    executionOrdersubList?:SubItem[];
+    
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+   // apiUrl: 'https://sd-dev.c-878bd9e.kyma.ondemand.com',
+    apiUrl: 'https://execution.dc5d803.kyma.ondemand.com',
+    clientID: "f73c32fb-86bc-4908-afb7-ee0a7144a67b",
+    clientSecret: "o7nRa6E3nXPQvwa=CUOQj1o5cBzLe:x"
+  };
